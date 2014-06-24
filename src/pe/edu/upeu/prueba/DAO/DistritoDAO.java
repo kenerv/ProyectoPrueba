5e0f19f0444e8e90b725d92239b8b000d23d7e81
alum.fial7 <alum.fial7@FIA-LAB7A2.lim.upeu.edu.pe>
2014-06-24 17:00:58 -0500

    versión 3

--- src/pe/edu/upeu/prueba/DAO/DistritoDAO.java
+++ src/pe/edu/upeu/prueba/DAO/DistritoDAO.java
@@ -6,10 +6,35 @@
 
 package pe.edu.upeu.prueba.DAO;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import pe.edu.upeu.prueba.config.Conexion;
+import pe.edu.upeu.prueba.modelo.Distrito;
+
 /**
  *
  * @author alum.fial7
  */
 public class DistritoDAO {
-    
+    private ArrayList<Distrito> lista = new ArrayList();
+    Distrito d = new Distrito();
+    private String sql;
+    private Statement st;
+    private Connection cx;
+    private ResultSet rs;
+    public ArrayList<Distrito> listarDistrito(){
+    sql = "SELEC *FROM distrito";
+        try {
+            cx = Conexion.getConexion();
+            st = cx.createStatement();
+            rs = st.executeQuery(sql);
+            while(rs.next()){
+                lista.add(Distrito.loadDistrito(rs));
+            }
+        } catch (Exception e) {
+        }    
+        return lista;
+    }
 }
